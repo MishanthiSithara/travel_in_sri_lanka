b5c8b6c4865b4495d14cb891cc5cc3ca8e0282f7
MishanthiSithara <sitharamishanthi@gmail.com>
2025-04-22 15:13:22 +0530

    about,book,destination,gallery,login,package,visit pages and images folder changes

---
+++ about.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+    <!--swiper css link-->
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+/>
+    <!--font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
+<!--external css link-->
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!--header section starts-->
+<section class="header">
+    <a href="home.php" class="logo">travelA+</a>
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="package.php">package</a>
+        <a href="book.php">book</a>
+        <a href="gallery.php">gallery</a>
+        <a href="destination.php">destination</a>
+        <a href="visit.php">visit</a>
+        <a href="login.php">login</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+    <!--header section end-->
+
+<div class="heading">
+    <h1>about us</h1>
+</div>
+
+
+
+
+
+
+<!--footer section starts-->
+
+<section class="footer">
+<div class="box-container">
+    <div class="box">
+        <h3>quick links</h3>
+        <a href="https://www.google.lk/maps/@8.2461758,80.644624,7z?entry=ttu&g_ep=EgoyMDI1MDQwMi4xIKXMDSoASAFQAw%3D%3D"> <i class="fas fa-angle-right"></i>site map</a>
+        <a href="https://complaints.sltda.gov.lk/"> <i class="fas fa-angle-right"></i>Complaints</a>
+        <a href="https://www.srilankan.com/en_uk/go?gclid=Cj0KCQiAgK2qBhCHARIsAGACuzkd1iaEyau_R2wKmGwSUPzPeUmWQ39csIDa_qL8lQiSdnczydbqDe0aAmfvEALw_wcB"> <i class="fas fa-angle-right"></i>Sri lankan airlines</a>
+        <a href="https://www.immigration.gov.lk/"> <i class="fas fa-angle-right"></i>immigration & emigration</a>
+        <a href="https://www.airport.lk/"> <i class="fas fa-angle-right"></i>airport & aviation services</a>
+    </div>
+
+    <div class="box">
+    <h3>Extra links</h3>
+    <a href="#"> <i class="fas fa-angle-right"></i>ask questions</a>
+    <a href="#"> <i class="fas fa-angle-right"></i>about us</a>
+    <a href="#"> <i class="fas fa-angle-right"></i>privacy policy</a>
+    <a href="#"> <i class="fas fa-angle-right"></i>term of use</a>
+    </div>
+
+    <div class="box">
+    <h3>contact info</h3>
+    <a href="#"> <i class="fas fa-phone"></i>+94758721478</a>
+    <a href="#"> <i class="fas fa-phone"></i>+94758721477</a>
+    <a href="#"> <i class="fas fa-envelope"></i>sitharami@gmail.com</a>
+    <a href="#"> <i class="fas fa-map"></i>colombo, sri lanka</a>
+    </div>
+
+    <div class="box">
+    <h3>follow us</h3>
+    <a href="#"> <i class="fab fa-facebook-f"></i>facebook</a>
+    <a href="#"> <i class="fab fa-twitter"></i>twitter</a>
+    <a href="#"> <i class="fab fa-instagram"></i>instagram</a>
+    <a href="#"> <i class="fab fa-linkedin"></i>linkedin</a>
+    </div>
+
+</div>
+<div class="credit">created by <span>mrs. A+ designer</span> | all rights reserved!</div>
+
+</section>
+
+
+<!--footer section ends-->
+
+
+
+
+
+
+<!--swiper js file link-->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!--js file link-->
+<script src="js/script.js"></script>
+</body>
+</html>
